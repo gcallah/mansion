fb26dc3d15015ac9b7f56198431588aefaaca1f9
gcallah <gcallah@mac.com>
2026-07-24 01:32:22 -0400

    Moved testapp req to github

--- dashboard/cl_dash_board.py
+++ dashboard/cl_dash_board.py
@@ -1,6 +1,24 @@
+import textapp.text_app as tapp
+
 from dashboard.dash_board import Dashboard
 
 
 class CLDashboard(Dashboard):
     def __init__(self, menu: str):
         self.menu = menu
+
+
+def main():
+    # Running form in test mode needs to be fixed!
+    ret = tapp.data_repr(tapp.TEST_DATA)
+    print(tapp.fmt.text(ret[tapp.DATA_TEXT]))
+    if tapp.mode == tapp.PROD:
+        mod_form = tapp.run_form(tapp.TEST_FORM)
+        # only the field values will go back to the server:
+        form_info = f"The modified form is: {mod_form[tapp.FLDS]}"
+        print(tapp.fmt.menu_choice(form_info))
+    return tapp.run_menu_cont(tapp.TEST_MENU)
+
+
+if __name__ == "__main__":
+    main()
